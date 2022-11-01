1d4bd6c8fc2aa92767db602be16e1911925a41da
rabbul <rabbulansary7@gmail.com>
2022-11-02 00:18:33 +0530

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,47 @@
+const userModel = require('../models/user.model');
+
 class UserController {
-    constructor() {}
+    constructor() { }
 
     /**
-     * @Method User Login Controller
+     * @Method User registerPage Controller
      * @param {*} req 
      * @param {*} res 
      */
-    async login(req, res) {
+    async registerPage(req, res) {
         try {
-            res.send('Hello');
+            res.send(`
+            <form action="register" method="post" enctype="multipart/form-data">
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname" value="John"><br>
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname" value="Doe"><br><br>
+            <input type="submit" value="Submit">
+            </form> 
+            `);
         } catch (error) {
-            return res.status(500).send({status: 500, message: error.message})
+            return res.status(500).send({ status: 500, message: error.message })
+        }
+    }
+
+
+    /**
+     * @Method User registerPage Controller
+     * @param {*} req 
+     * @param {*} res 
+     */
+    async register(req, res) {
+        try {
+            if (!req.body.username || !req.body.password) {
+                return res.status(400).send({ status: 400, message: "Username and password are required!" });
+            }
+            let user = await User.create({ username: req.body.username, password: req.body.password });
+            if (!user) {
+                return null;
+            }
+            return res.status(200).send({ status: 200, message: "User registered successfully.", data: user });
+        } catch (error) {
+            return res.status(500).send({ status: 500, message: error.message })
         }
     }
 }
--- src/routes/api/user.route.js
+++ src/routes/api/user.route.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const controllers = require('../../controllers/user.controller');
+const multer = require('multer');
+const request_param = multer();
 
-router.all('/*', auth.authenticateAPI);
-router.post('/login', controllers.login);
-// router.post('/register', controllers.register);
+// router.all('/*', auth.authenticateAPI);
+// router.get('/login', controllers.loginPage);
+// router.post('/login', controllers.login);
+
+
+router.get('/register', controllers.registerPage);
+router.post('/register', request_param.any(), controllers.register);
 
 module.exports = router;
